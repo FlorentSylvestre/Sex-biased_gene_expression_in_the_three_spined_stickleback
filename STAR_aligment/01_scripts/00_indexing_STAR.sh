def6bcd6017077bd75d075720410b6c10c8e857e
Florent Sylvestre <92882895+FlorentSylvestre@users.noreply.github.com>
2024-06-03 19:14:22 -0400

    Create 00_indexing_STAR.sh

---
+++ STAR_aligment/01_scripts/00_indexing_STAR.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+########################################
+## Global variables
+########################################
+
+GENOME="03_reference/genome"
+GFF=$2
+NCPUS=6
+CSD="/prg/picard-tools/1.119/CreateSequenceDictionary.jar"
+
+
+########################################
+## Execute script ##
+########################################
+
+samtools faidx ${GENOME}
+
+#Build reference
+echo "Building ${GENOME} index..."
+
+if [ ! -e  ${GENOME%.*}.dict ]
+then
+        java -jar "$CSD" \
+    R="$GENOME"_F.fa \
+    O="$GENOME"_F.dict
+
+    java -jar "$CSD" \
+    R="$GENOME"_M.fa \
+    O="$GENOME"_M.dict
+fi
+
+BIT_SIZE=20
+echo "STAR indexing \n"
+STAR --runMode genomeGenerate \
+    --runThreadN ${NCPUS} \
+    --genomeDir "03_reference/STARindex_F/" \
+    --genomeFastaFiles ${GENOME}_F.fa \
+    --sjdbGTFfile ${GFF} \
+    --sjdbGTFtagExonParentTranscript Parent \
+    --sjdbGTFfeatureExon exon \
+    --genomeChrBinNbits ${BIT_SIZE} \
+    --genomeSAindexNbases 13
+
+STAR --runMode genomeGenerate \
+    --runThreadN ${NCPUS} \
+    --genomeDir "03_reference/STARindex_M/" \
+    --genomeFastaFiles ${GENOME}_M.fa \
+    --sjdbGTFfile ${GFF} \
+    --sjdbGTFtagExonParentTranscript Parent \
+    --sjdbGTFfeatureExon exon \
+    --genomeChrBinNbits ${BIT_SIZE} \
+    --genomeSAindexNbases 13
